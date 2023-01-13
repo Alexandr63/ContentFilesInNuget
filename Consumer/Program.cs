9d50d3af31725a8d46fa0f5ff25769e8786e6996
Alexandr Kononov <a.kononov@tinkoff.ru>
2023-01-13 18:59:08 +0400

    Test of getting content from NuGet package.

---
+++ Consumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NugetLib;
+
+namespace Consumer
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Root.Test();
+        }
+    }
+}
---
+++ NugetLib/Root.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System;
+
+namespace NugetLib
+{
+    public class Root
+    {
+        public static void Test()
+        {
+            string dir = "fonts";
+            bool hasFonts = Directory.Exists(dir) && Directory.GetFiles(dir).Length == 2;
+
+            Console.WriteLine($"Has fonts: {hasFonts}");
+        }
+    }
+}
